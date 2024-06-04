d0104a35ae67795a7c7a57b82020a626193147c7
oriakuemeka <ucintonet@gmail.com>
2024-06-04 15:39:54 +0000

    Run Bundle install, update create and product

--- challenge_create.rb
+++ challenge_create.rb
@@ -1,5 +1,7 @@
+load 'ar.rb'
+
 # Method 1: Using new and save
-product1 = Product.new(name: 'Product1', price: 15.99, stock_quantity: 10)
+product1 = Product.new(name: 'Product1', description: "item", price: 15.99, stock_quantity: 10)
 if product1.save
   puts 'Product1 saved successfully'
 else
@@ -7,11 +9,12 @@ else
 end
 
 # Method 2: Using create
-product2 = Product.create(name: 'Product2', price: 20.99, stock_quantity: 8)
+product2 = Product.create(name: 'Product2', description: "item", price: 20.99, stock_quantity: 8)
 
 # Method 3: Using new with a block
 product3 = Product.new do |p|
   p.name = 'Product3'
+  p.description = "item"
   p.price = 25.99
   p.stock_quantity = 5
 end
@@ -22,7 +25,7 @@ else
 end
 
 # Creating a Product with missing columns
-invalid_product = Product.new(name: 'InvalidProduct')
+invalid_product = Product.new(name: 'Inv')
 if invalid_product.save
   puts 'InvalidProduct saved successfully'
 else
--- models/product.rb
+++ models/product.rb
@@ -1,8 +1,13 @@
 class Product < ActiveRecord::Base
   # This AR object is linked with the products table.
-  
+
   # A product has a many to one relationship with a category.
   # The products table has a category_id foreign key.
   # In other words, a product belongs to a category.
+  # Columns: id, name, description, price, stock_quantity, category_id, created_at, updated_at
+  # Association: Belongs to category (category_id)
   belongs_to :category
+
+  validates :name, presence: true, uniqueness: true, length: { minimum: 4 }
+  validates :description, :price, :stock_quantity, presence: true
 end
